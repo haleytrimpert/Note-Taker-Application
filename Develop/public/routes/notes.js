83a69808622c3ff149a8edef5470cc4181e14b1b
Haley T <hntrimpert@yahoo.com>
2023-03-23 17:34:56 -0400

    fix post, finish delete

--- Develop/public/routes/notes.js
+++ Develop/public/routes/notes.js
@@ -1,36 +1,72 @@
 const noteR = require("express").Router();
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
+const readFromFile = util.promisify(fs.readFile);
+let db = require("../../db/db.json");
 
 noteR.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "../notes.html"))
 );
 
 noteR.get("/api/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "../../db/db.json"))
+  //res.sendFile(path.join(__dirname, "../../db/db.json"))
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
 );
+
+//let array = [];
 
 noteR.post("/api/notes", (req, res) => {
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: db.length + 1,
+  };
+  console.log(db);
   //hit the save button,request info sent to bd.json, apply to notes.html
-  fs.readFile(path.join(__dirname, "../../db/db.json"), (error, data) => {
-    const json = JSON.parse(data);
-    json.push(req.body);
-    fs.writeFile(
-      path.join(__dirname, "../../db/db.json"),
-      JSON.stringify(json),
-      (error) => {
-        if (error) {
-          console.log(error);
-          return;
-        }
+  // fs.readFile(path.join(__dirname, "../../db/db.json"), (error, data) => {
+  //   //const json = JSON.parse(data);
+  //   array = JSON.parse(data);
+  //   //json.push(req.body);
+  //   console.info(req.body);
+  //   return array;
+  // });
+  db.push(newNote);
+  console.log(db);
+  fs.writeFile(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify(db),
+    (error) => {
+      if (error) {
+        console.log(error);
+        return;
       }
-    );
-  });
-  return res.sendFile(path.join(__dirname, "../../db/db.json"));
+    }
+  );
+  //json.push(newNote);
+
+  res.send(db);
 });
 
-noteR.delete("/api/notes", (req, res) =>
-  res.deleteFile(path.join(__dirname, "../../db/db.json"))
-);
+noteR.delete("/api/notes/:id", (req, res) => {
+  let deleteId = req.params.id;
+  let deleteNote = db.filter((note) => {
+    return note.id != deleteId;
+  });
+  db = deleteNote;
+  console.log(deleteId);
+  console.log(deleteNote);
+  fs.writeFile(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify(db),
+    (error) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+    }
+  );
+});
 
 module.exports = noteR;
