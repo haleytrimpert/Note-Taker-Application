810441d3b87203a0be9eb9428d2703142d70081b
Haley T <hntrimpert@yahoo.com>
2023-03-26 18:11:12 -0400

    fixed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const routes = require('./public/routes/index');
 //const notes = express.Router();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
